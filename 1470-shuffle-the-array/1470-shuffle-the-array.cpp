6de0e850ed7beadcd864ca44a72e855110fc9349
SWETHA-DHAVALA <24B11AI087@adityauniversity.in>
2026-05-21 09:52:03 +0530

    Time: 0 ms (100%), Space: 13.5 MB (16.46%) - LeetHub

--- 1470-shuffle-the-array/1470-shuffle-the-array.cpp
+++ 1470-shuffle-the-array/1470-shuffle-the-array.cpp
@@ -1,24 +1,36 @@
-class Solution {
-public:
-    vector<int> shuffle(vector<int>& nums, int n) {
-        vector<int>s1(nums.begin(),nums.begin()+n);
-        vector<int>s2(nums.begin()+n,nums.end());
-        vector<int>ans;
-        int j = 0 , k = 0;
-        for(int i = 0 ; i < nums.size(); i++)
-        {
-            if(i%2==0)
-            {
-                ans.push_back(s1[j]);
-                j++;
-            }
-            else
-            {
-                ans.push_back(s2[k]);
-                k++;
-            }
+// class Solution {
+// public:
+//     vector<int> shuffle(vector<int>& nums, int n) {
+//         vector<int>s1(nums.begin(),nums.begin()+n);
+//         vector<int>s2(nums.begin()+n,nums.end());
+//         vector<int>ans;
+//         int j = 0 , k = 0;
+//         for(int i = 0 ; i < nums.size(); i++)
+//         {
+//             if(i%2==0)
+//             {
+//                 ans.push_back(s1[j]);
+//                 j++;
+//             }
+//             else
+//             {
+//                 ans.push_back(s2[k]);
+//                 k++;
+//             }
 
+//         }
+//         return ans;
+//     }
+// };
+class Solution {
+    public:
+        vector<int> shuffle(vector<int>& nums , int n){
+            vector<int>ans;
+            for(int i = 0 ; i < n ; i++)
+            {
+                ans.push_back(nums[i]);
+                ans.push_back(nums[i+n]);
+            }
+            return ans;
         }
-        return ans;
-    }
 };
