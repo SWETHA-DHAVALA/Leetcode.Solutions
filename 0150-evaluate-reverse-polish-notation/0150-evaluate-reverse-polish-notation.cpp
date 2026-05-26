95c3dc1071565419815dec9be3b678c412e2545e
SWETHA-DHAVALA <24B11AI087@adityauniversity.in>
2026-05-26 08:52:24 +0530

    Time: 0 ms (100%), Space: 16.9 MB (85.96%) - LeetHub

---
+++ 0150-evaluate-reverse-polish-notation/0150-evaluate-reverse-polish-notation.cpp
@@ -0,0 +1,31 @@
+class Solution {
+public:
+    int evalRPN(vector<string>& tokens) {
+        stack<int>s;
+        for(int i = 0 ; i < tokens.size();i++)
+        {
+            if(tokens[i]!="+"&&tokens[i]!="-"&&tokens[i]!="*"&&tokens[i]!="/"){
+                int j = stoi(tokens[i]);
+                s.push(j);
+            }
+            
+            else
+            {
+                int b = s.top();
+                s.pop();
+                int a = s.top();
+                s.pop();
+                if(tokens[i]=="+")
+                s.push(a+b);
+                else if(tokens[i]=="-")
+                s.push(a-b);
+                else if(tokens[i]=="*")
+                s.push(a*b);
+                else 
+                s.push(a/b);
+            }
+        }
+        return s.top();
+
+    }
+};
