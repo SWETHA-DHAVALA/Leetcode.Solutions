caf60335265cecf8bbab3c4168f2e724514666cf
SWETHA-DHAVALA <24B11AI087@adityauniversity.in>
2026-05-31 06:37:30 +0530

    Time: 31 ms (66.42%), Space: 106.6 MB (99.12%) - LeetHub

---
+++ 2126-destroying-asteroids/2126-destroying-asteroids.cpp
@@ -0,0 +1,17 @@
+class Solution {
+public:
+    bool asteroidsDestroyed(int mass, vector<int>& asteroids) {
+        sort(asteroids.begin(),asteroids.end());
+        long long z = mass;
+        for(int i = 0 ; i < asteroids.size() ; i++)
+        {
+            if(z >= asteroids[i])
+            {
+                z += asteroids[i];
+            }
+            else 
+            return false;
+        }
+        return true;
+    }
+};
